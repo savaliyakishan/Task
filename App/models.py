ab5cb8b35b7fcd747e8432720ba5e0d24b932613
kishan savaliya <savaliyakishan9083@gmail.com>
2023-03-28 10:20:51 +0530

    change fields

--- App/models.py
+++ App/models.py
@@ -7,5 +7,5 @@ class city(models.Model):
 class Component(models.Model):
     id = models.AutoField(primary_key=True,db_column="Id")
     name = models.CharField(max_length=255,null=False,blank=False,db_column="Name")
-    phone = models.IntegerField(null=False,blank=False,db_column="Phone_Number")
+    phone = models.BigIntegerField(null=False,blank=False,db_column="Phone_Number")
     city = models.ManyToManyField(city)
