204b1a3d3dd26396a0ed2388db81ba5170e17c39
kishan savaliya <savaliyakishan9083@gmail.com>
2023-03-28 10:03:59 +0530

    first commit

---
+++ App/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class city(models.Model):
+    city = models.CharField(max_length=255,null=False,blank=False,db_column="City")
+
+class Component(models.Model):
+    id = models.AutoField(primary_key=True,db_column="Id")
+    name = models.CharField(max_length=255,null=False,blank=False,db_column="Name")
+    phone = models.IntegerField(null=False,blank=False,db_column="Phone_Number")
+    city = models.ManyToManyField(city)
